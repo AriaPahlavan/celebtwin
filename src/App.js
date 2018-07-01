4b1ce2420378b6ee812934917ca05ac853c7fc9d
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-01 15:48:12 -0500

    add background particles component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    move: {
+      speed: 1
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='below' params={particleOptions} />
         <Navigation />
         <Logo />
         <Rank />
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+
+const formStyle = {
+  width: '700px',
+  background: 'linear-gradient(to right, rgb(219, 197, 193), rgb(180, 214, 203))'
+};
+
 const ImageLinkForm = () => {
   return (
     <div>
       <p className='f3'>
         {'Try detecting faces in your images!'}
       </p>
-      <div className='center pa4 br3  shadow-5' style={{width: '700px'}}>
+      <div className='center pa4 br3  shadow-5' style={formStyle}>
         <input className='f4 pa2 w-70 center' type='text'/>
         <button className='w-30 grow f4 link ph3 pv2 dib bg-washed-red'>detect</button>
       </div>
