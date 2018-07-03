1a0b43c1f76b82e32f31ddf885629af0061c84cb
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-02 19:32:03 -0500

    add some error handling

--- src/App.js
+++ src/App.js
@@ -143,13 +143,12 @@ class App extends Component {
     fetch('http://localhost:3000/image', {
       method: 'put',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        id: this.state.user.id
-      })
+      body: JSON.stringify({id: this.state.user.id})
     })
       .then(response => response.json())
       .then(count => this.setState(Object
-                         .assign(this.state.user, {entries: count})));
+                         .assign(this.state.user, {entries: count})))
+      .catch(console.log);
     return data;
   }
 
