215c53fe2b0ff552b9be24bf5980589d6108b3d5
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-16 18:46:00 -0500

    fix issue with resposiveness of image link form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,29 +2,32 @@ import React from 'react';
 
 
 const formStyle = {
-  width: '700px',
   background: 'linear-gradient(to right, rgb(219, 197, 193), rgb(180, 214, 203))'
 };
 
 const ImageLinkForm = ({onInputChange, onDetectClick}) => {
   return (
-    <div>
-      <div>
-        <p className='f3'>
-          {'Try detecting faces in your images!'}
-        </p>
-        <div className='center pa4 br3  shadow-5' style={formStyle}>
-          <input className='f4 pa2 w-70 center' type='text'
-                 onChange={onInputChange}/>
-          <input className='w-30 grow f4 link ph3 pv2 dib bg-washed-red'
-                  onClick={onDetectClick}
-                  type="submit"
-                  value="detect"
-          />
-        </div>
+    <div className='pl4-l pr4-l'>
+      <div className='mw7 center pl4 pr4'>
+        <fieldset class="cf bn ma0 pa0">
+          <p className='f3'>
+            {'Try detecting faces in your images!'}
+          </p>
+          <div className='center pa4 br3  shadow-5' style={formStyle}>
+            <input className='f4 pa2 w-70 center' type='text'
+                   onChange={onInputChange}/>
+            <input className='w-30 grow f4 link ph3 pv2 dib bg-washed-red'
+                    onClick={onDetectClick}
+                    type="submit"
+                    value="detect"
+            />
+          </div>
+        </fieldset>
       </div>
     </div>
+
+
+
   );
 };
-
 export default ImageLinkForm;
