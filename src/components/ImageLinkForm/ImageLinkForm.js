64f4217b014816b9bca06c04ee2fdf760e3ab367
Aria Pahlavan <apahlavan1@utexas.edu>
2018-07-24 20:37:27 -0500

    add image URL input placeholder

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -18,6 +18,7 @@ const ImageLinkForm = ({onInputChange, onDetectClick}) => {
 
             <input className='f4 pa2 w-70 center'
                    type='text'
+                   placeholder='Image URL'
                    onChange={onInputChange}/>
 
             <input className='w-30 grow f4 link pa2 dib bg-washed-red'
